2ac1fedcca3c924bf9b0b6f335624a77ea9b4a0d
adrianrezha29 <adrianrezha29@gmail.com>
2022-12-02 18:09:52 +0700

    add case 2

--- StackUsingLinkedList/Program.cs
+++ StackUsingLinkedList/Program.cs
@@ -83,6 +83,14 @@ namespace StackUsingLinkedList
                         int num = Convert.ToInt32(Console.ReadLine());
                         s.push(num);
                         break;
+                    case '2':
+                        if (s.empty())
+                        {
+                            Console.WriteLine("\nStack empty");
+                            break;
+                        }
+                        s.pop();
+                        break;
                 }
             }
         }
